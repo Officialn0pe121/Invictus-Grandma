4c185d4cedd7725b9a3eecf88d8bfbf991651d4f
N0pe <126282829+Officialn0pe121@users.noreply.github.com>
2023-07-12 18:10:52 -0400

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,1 +1,21 @@
+-- Resource Metadata
+fx_version 'cerulean'
+game 'gta5'
 
+lua54 'yes'
+
+author 'N0pe'
+description 'Invictus-Grandma'
+version '1.0.0'
+
+client_scripts {
+    'client/*.lua',
+}
+
+server_scripts {
+    'server/*.lua',
+}
+
+dependencies {
+    'qb-core'
+}
