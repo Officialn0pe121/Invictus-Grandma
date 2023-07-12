94c70335cff153ae82f369aaa4c30d5b2e12d6b7
N0pe <126282829+Officialn0pe121@users.noreply.github.com>
2023-07-12 18:27:34 -0400

    Update sv_main.lua

--- server/sv_main.lua
+++ server/sv_main.lua
@@ -1,7 +1,6 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 
 local GrandmaFee = 2000
-local AiDocFee = 5000
 local CDTimer = 10 -- Cooldown in minutes
 local CD = false
 local debug = true
@@ -10,7 +9,7 @@ local payType = "cash" -- "cash" / "bank" / "crypto"
 local Cooldown = function()
     CD = true
     if debug then
-        print("Grandma Cooldown")
+        print("Grandma Cooldown Started")
     end
 	local timer = CDTimer * 60000
 	while timer > 0 do
@@ -18,6 +17,9 @@ local Cooldown = function()
 		timer = timer - 1000
 		if timer == 0 then
 			CD = false
+			if debug then
+        			print("Grandma Cooldown Ended")
+    			end
 		end
 	end
 end
@@ -25,17 +27,44 @@ end
 QBCore.Functions.CreateCallback('Invictus-Grandma:server:RevivePlayer', function(source, cb)
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
-
-    if Player.PlayerData.money.cash >= GrandmaFee then
-        if not CD then
-            Player.Functions.RemoveMoney(payType, GrandmaFee, "Grandma Visit")
-            cb(true)
-            Cooldown()
+    if payType == "cash" then
+        if Player.PlayerData.money.cash >= GrandmaFee then
+            if not CD then
+                Player.Functions.RemoveMoney("cash", GrandmaFee, "Grandma Visit")
+                cb(true)
+                Cooldown()
+            else
+                TriggerClientEvent('QBCore:Notify', src, "Grandma doesn\'t have any band-aids right now..", "error",  5000)
+            end
         else
-            TriggerClientEvent('QBCore:Notify', src, "Grandma doesn\'t have any band-aids right now..", "error",  5000)  -- Default Notify
+            TriggerClientEvent('QBCore:Notify', src, "Grandma isn\'t interested in helping..", "error",  5000)
+            cb(false)
+        end
+    elseif payType == "bank" then
+        if Player.PlayerData.money.bank >= GrandmaFee then
+            if not CD then
+                Player.Functions.RemoveMoney("bank", GrandmaFee, "Grandma Visit")
+                cb(true)
+                Cooldown()
+            else
+                TriggerClientEvent('QBCore:Notify', src, "Grandma doesn\'t have any band-aids right now..", "error",  5000)
+            end
+        else
+            TriggerClientEvent('QBCore:Notify', src, "Grandma isn\'t interested in helping..", "error",  5000)
+            cb(false)
         end
     else
-        TriggerClientEvent('QBCore:Notify', src, "Grandma isn\'t interested in helping..", "error",  5000)
-        cb(false)
+        if Player.PlayerData.money.crypto >= GrandmaFee then
+            if not CD then
+                Player.Functions.RemoveMoney("crypto", GrandmaFee, "Grandma Visit")
+                cb(true)
+                Cooldown()
+            else
+                TriggerClientEvent('QBCore:Notify', src, "Grandma doesn\'t have any band-aids right now..", "error",  5000)
+            end
+        else
+            TriggerClientEvent('QBCore:Notify', src, "Grandma isn\'t interested in helping..", "error",  5000)
+            cb(false)
+        end
     end
 end)
