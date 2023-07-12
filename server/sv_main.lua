cb066a7283b7facad14e63e79bac4c7fcc67d1e8
N0pe <126282829+Officialn0pe121@users.noreply.github.com>
2023-07-12 18:16:46 -0400

    Update sv_main.lua

--- server/sv_main.lua
+++ server/sv_main.lua
@@ -5,6 +5,7 @@ local AiDocFee = 5000
 local CDTimer = 10 -- Cooldown in minutes
 local CD = false
 local debug = true
+local payType = "cash" -- "cash" / "bank" / "crypto"
 
 local Cooldown = function()
     CD = true
@@ -27,41 +28,14 @@ QBCore.Functions.CreateCallback('Invictus-Grandma:server:RevivePlayer', function
 
     if Player.PlayerData.money.cash >= GrandmaFee then
         if not CD then
-            Player.Functions.RemoveMoney('cash', GrandmaFee, "Grandma Visit")
+            Player.Functions.RemoveMoney(payType, GrandmaFee, "Grandma Visit")
             cb(true)
             Cooldown()
         else
-            TriggerClientEvent('QBCore:Notify', src, "Grandma doesn\'t have any band-aids right now..", "error",  5000)
+            TriggerClientEvent('QBCore:Notify', src, "Grandma doesn\'t have any band-aids right now..", "error",  5000)  -- Default Notify
         end
     else
         TriggerClientEvent('QBCore:Notify', src, "Grandma isn\'t interested in helping..", "error",  5000)
-        cb(false)
-    end
-end)
-
-QBCore.Functions.CreateCallback('Invictus-Grandma:docOnline', function(source, cb)
-    local src = source
-    local Player = QBCore.Functions.GetPlayer(src)
-    local xPlayers = QBCore.Functions.GetPlayers()
-    local doctor = 0
-
-    if Player.PlayerData.money.cash >= AiDocFee then
-        for i=1, #xPlayers, 1 do
-            local xPlayer = QBCore.Functions.GetPlayer(xPlayers[i])
-            if xPlayer.PlayerData.job.name == 'ambulance' and xPlayer.PlayerData.job.onduty then
-                doctor = doctor + 1
-            end
-        end
-        if doctor <= 1 then
-            Player.Functions.RemoveMoney('cash', AiDocFee, "Ambulance Visit")
-            cb(true)
-        else
-            TriggerClientEvent('QBCore:Notify', src, "Too many doctors online..", "error",  5000)
-            cb(false)
-        end
-
-    else
-        TriggerClientEvent('QBCore:Notify', src, "Not enough money for the ambulance..", "error",  5000)
         cb(false)
     end
 end)
